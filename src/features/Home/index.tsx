79ee7a538b4dcba58137dc398c5cb8ab4e8663b3
PhungLuuGiaHuy <86343417+PhungLuuGiaHuy@users.noreply.github.com>
2022-02-26 16:53:45 +0700

    trang chủ
    đang làm trang chủ

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+
 import Header from '../../components/header/header';
+import logo from '../../asset/logo.png';
+import React, { Component } from "react";
+import Slider from "react-slick";
+import './body.scss'
 const Home: any = () => {
     return (
         <div className="">
             <Header />
+            <div className=''>
+         <div  className=''><img src={logo} alt="logo2"className='logo2' /> </div>
+            <div className='body'>
+                <h1 className='label1'>C&T</h1>
+                <h2 className='label2'>GIẢI PHÁP CHO MỌI THANH TOÁN ONLINE</h2>
+            </div>
+  
+        </div>
         </div>
     );
+   
 }
-
-export default Home;
+export default class SimpleSlider extends Component {
+    render() {
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+export default Home;}
