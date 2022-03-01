1b5db24eb9f6c14de9e6983e9040b5f7fe7000e6
PhungLuuGiaHuy <86343417+PhungLuuGiaHuy@users.noreply.github.com>
2022-03-01 16:23:15 +0700

    xong slide autoplay
    xong slide autoplay

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -2,31 +2,69 @@
 import Header from '../../components/header/header';
 import logo from '../../asset/logo.png';
 import React, { Component } from "react";
-import Slider from "react-slick";
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css"; 
+  import "slick-carousel/slick/slick-theme.css";
 import './body.scss'
+
 const Home: any = () => {
-    return (
+   
+    return (      
+            
+     
         <div className="">
             <Header />
+            <br/>
+            <br/>
+            <br/>
             <div className=''>
          <div  className=''><img src={logo} alt="logo2"className='logo2' /> </div>
             <div className='body'>
                 <h1 className='label1'>C&T</h1>
-                <h2 className='label2'>GIẢI PHÁP CHO MỌI THANH TOÁN ONLINE</h2>
+                <h2 className='label2'>GIẢI PHÁP CHO MỌI THANH TOÁN ONLINE</h2>             
             </div>
-  
+            <div className='slider'>
+              <h2> Công Ty C&T </h2>
+              <Slider {...settings}>
+                <div className='slider'>
+                <img src={logo} alt="logo2"className='logo2' />
+                </div>
+                <div>
+                <img src={logo} alt="logo2"className='logo2' />
+                </div>
+                <div>
+                <img src={logo} alt="logo2"className='logo2' />
+                </div>
+                <div>
+                <img src={logo} alt="logo2"className='logo2' />
+                </div>
+                <div>
+                <img src={logo} alt="logo2"className='logo2' />
+                </div>
+                <div>
+                <img src={logo} alt="logo2"className='logo2' />
+                </div>
+              </Slider>
+            </div>
         </div>
         </div>
     );
    
+    }
+
+ 
+    
+     
+   
+export default Home  ;
+var settings = {
+    dots: true,
+    infinite: true,
+    autoplay:true,
+    autoplaySpeed:1500,
+    centerMode:true,
+    speed: 1000,
+  
+    slidesToShow: 1,
+    slidesToScroll: 1
 }
-export default class SimpleSlider extends Component {
-    render() {
-      const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
-export default Home;}
