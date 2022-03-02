5a909662347d7c76a887b1387b41db673d1af4cd
baoquoc270899 <82484570+baoquoc270899@users.noreply.github.com>
2022-03-02 16:53:56 +0700

    commit view product

--- src/features/product/component/dasboard/dasboard.tsx
+++ src/features/product/component/dasboard/dasboard.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Banner from '../banner/banner';
 import './dasboard.scss';
+import kioskimg from '../../../../asset/kioskimg.png';
+import maythunganimg from '../../../../asset/maythunganimg.png';
+import softwareimg from '../../../../asset/softwareimg.png';
+
 const Dasboard: any = () => {
     return (
         <div className="">
@@ -21,13 +25,44 @@ const Dasboard: any = () => {
                     </div>
                     <div className='div-btn'>
                         <button className='iconn btnmaythungan'></button>
-                        <p className='p-items'>Máy thu ngân</p>
+                        <p className='p-items'>Máy Thu Ngân</p>
                     </div>
                     <div className='div-btn'>
                         <button className='iconn btnphanmem'></button>
-                        <p className='p-items'>Phần mềm</p>
+                        <p className='p-items'>Phần Mềm</p>
                     </div>
                 </div>
+                <br />
+                <br />
+                <div className='product'>
+                    <div className='product-card'>
+                        <div className='fix'>
+                            <img src={kioskimg} alt="kioskimg" />
+                        </div>
+                        <p className='product-name'>Kiosk</p>
+                        <p className='detail'>Chi Tiết</p>
+                        <button className='btn-contact'>Liên Hệ Ngay</button>
+                    </div>
+                    <div className='product-card'>
+                        <div className='fix'>
+                            <img src={maythunganimg} alt="maythunganimg" />
+                        </div>
+                        <p className='product-name'>Máy Thu Ngân</p>
+                        <p className='detail'>Chi Tiết</p>
+                        <button className='btn-contact'>Liên Hệ Ngay</button>
+                    </div>
+                    <div className='product-card'>
+                        <div className='fix'>
+                            <img src={softwareimg} alt="softwareimg" />
+                        </div>
+                        <p className='product-name'>Phần mềm</p>
+                        <p className='detail'>Chi tiết</p>
+                        <button className='btn-contact'>Liên Hệ Ngay</button>
+                    </div>
+                    
+                </div>
+                <br />
+                <br />
             </body>
         </div>
     );
